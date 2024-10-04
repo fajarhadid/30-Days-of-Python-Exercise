e25e6b72cada0c906862c9f4b25b13db5f88afaf
fajarhadid <fhadidarizal@gmail.com>
2024-10-04 16:17:52 +0700

    add exercise 3 level 1 (number comparison)

---
+++ Day_09_Conditionals/Level 1/exercise_3.py
@@ -0,0 +1,9 @@
+a = int(input("Enter number one: "))
+b = int(input("Enter number two: "))
+
+if a > b:
+    print(f"{a} is greater than {b}")
+elif a < b:
+    print(f"{a} is smaller than {b}")
+else:                       
+    print(f"{a} is equal to {b}")
