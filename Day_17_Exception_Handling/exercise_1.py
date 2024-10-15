0eafbf3e74692d785b0f703f57ad6f883e0a2a12
fajarhadid <fhadidarizal@gmail.com>
2024-10-15 14:37:25 +0700

    add day 17 exercise

---
+++ Day_17_Exception_Handling/exercise_1.py
@@ -0,0 +1,5 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+*nordic_countries, es, ru = names
+print(nordic_countries) # ['Finland', 'Sweden', 'Norway','Denmark','Iceland']
+print(es) # Estonia
+print(ru) # Russia
